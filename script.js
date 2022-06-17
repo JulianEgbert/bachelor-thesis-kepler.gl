3845223a0a4aadef29c8dfea106c76dc83090ba3
Julian <57799997+JulianEgbert@users.noreply.github.com>
2022-06-17 21:52:47 +0200

    First version of the tool (#1)
    * add testfile
    
    * first prototype
    
    * Add basic config
    
    * Simple prototype
    
    * Clean css
    
    * default dark background color
    
    * Move mapbox token to config
    
    * new token and remove debug logs
    
    * Improve css and overall structure
    
    * add a new file to visualizations
    
    * Update README and add Link to Repo
    
    * Prepare for first release
    
    * Prepare CONFIG_URL
    
    Co-authored-by: JulianEgbert <>

---
+++ script.js
@@ -0,0 +1,61 @@
+const CONFIG_URL = "https://raw.githubusercontent.com/JulianEgbert/bachelor-thesis-kepler.gl/main/config.json";
+var config = "";
+var loadedFilename = "";
+
+function loadConfig(configUrl = CONFIG_URL) {
+    fetch(configUrl).then(function (response) {
+        return response.json();
+    }).then(configLoaded);
+}
+
+function configLoaded(newConfig) {
+    config = newConfig;
+    const selectEl = document.getElementById("fileSelect");
+    config.files.forEach((file) => {
+        const option = document.createElement("option");
+        option.value = file.path;
+        option.innerHTML = file.name;
+        selectEl.appendChild(option);
+    });
+}
+
+loadConfig();
+
+function loadFromInput() {
+    const inputField = document.getElementById("fileSelect");
+    if (inputField.disabled || inputField.value == "") {
+        return;
+    }
+    const filename = inputField.value;
+    if (filename == loadedFilename)
+        return;
+    try {
+        loadKeplerFromFilepath(filename);
+        loadedFilename = filename;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function loadKeplerFromFilepath(filepath) {
+    const url = `${config.baseUrl}${config.branch}/${filepath}`;
+    loadKeplerFromUrl(url);
+}
+
+function loadKeplerFromUrl(url) {
+    fetch(url).then(function (response) {
+        return response.text();
+    }).then(function (html) {
+        document.getElementById("kepler.gl-content").srcdoc = parseHTML(html);
+    });
+}
+
+function parseHTML(html) {
+    // Inject a valid MAPBOX_TOKEN here.
+    if (!config || !config.mapboxToken) {
+        window.alert("No mapbox token provided in config file.");
+        return html;
+    }
+    html = html.replace("'PROVIDE_MAPBOX_TOKEN'", `'${config.mapboxToken}'`);
+    return html;
+}
